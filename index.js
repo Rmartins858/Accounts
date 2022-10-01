c6ade83d6bdcb1b04301aea7eb6964203b65e527
Rafael Martins <rmartins858@gmail.com>
2022-10-01 20:39:41 -0300

    feat: project startup

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 // modulos externos
 
-const inquirer = require('inquirer')
-const chalk = require('chalk')
-
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 
 // modulos internos
 
-const fs = require('fs')
+const fs = require('fs');
+
+opration();
+
+function opration() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'Oque você deseja fazer?',
+        choices: [
+          'Criar conta',
+          'Consultar Saldo',
+          'Depositar',
+          'Sacar',
+          'Sair',
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer['action'];
+
+      console.log(action);
+    })
+    .catch((error) => console.log(error));
+}
 
 console.log('Iniciamos o Accounts');
