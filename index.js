439c1e1e764907f752886fa4f0a4120c1072bff5
Rafael Martins <rmartins858@gmail.com>
2022-10-01 20:48:05 -0300

    feat: Finishing account creation

--- index.js
+++ index.js
@@ -28,9 +28,19 @@ function opration() {
     .then((answer) => {
       const action = answer['action'];
 
-      console.log(action);
+      if(action === 'Criar conta'){
+        createAccount()
+      }
     })
     .catch((error) => console.log(error));
+}
+
+// create an account
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir'));
+
 }
 
 console.log('Iniciamos o Accounts');
